3608881bc72d498d5531307c88b9e228fa16d24c
tonymichaelhead <tonymichaelhead@gmail.com>
2018-12-02 07:34:56 -0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Game options/settings
+TITLE = "My Game"
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+
+# Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0,0)
+GREEN = (0, 255,0)
+BLUE = (0, 0, 255)
